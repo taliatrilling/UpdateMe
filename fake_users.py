268e36a3fb178ca255d1696f203d75f2c4c1779b
Talia Trilling <taliatrilling@gmail.com>
2016-11-07 21:17:14 +0000

    added functions allowing for bulk adding fake users and updates

--- fake_users.py
+++ fake_users.py
@@ -3,33 +3,33 @@ import factory
 from faker import Factory as FakerFactory
 from datetime import datetime
 from server import app
+import random
 
 #see docs for Faker at https://faker.readthedocs.io/en/latest/
 #see docs for factory_boy at https://factoryboy.readthedocs.io/en/latest/
 
 faker = FakerFactory.create()
 
-class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
-	"""Creates fake users"""
 
-	class Meta:
-		model = User
-		sqlalchemy_session = db.session
+def add_users():
+	"""Adds fake users to db, in theory should add 500 but usually fails before it can reach 500 
+	because the system will try to use the same fake username"""
 
-	username = faker.user_name()
-	password = faker.word()
-	joined_at = faker.date_time_this_year(before_now=True, after_now=False, tzinfo=None)
-	is_public = faker.boolean()
+	for i in range(500):
+		user = User(username=faker.user_name(), password=faker.word(), 
+			joined_at=faker.date_time_this_year(before_now=True, after_now=False, tzinfo=None),
+			is_public=faker.boolean())
+		db.session.add(user)
+		db.session.commit()
+
+def add_updates():
+	"""Adds fake updates to the system"""
+
+	current_users = db.session.query(User.user_id).all()
+	for i in range(500):
+		update = Update(user_id=random.choice(current_users), update_body=faker.text(max_nb_chars=140), 
+			posted_at=datetime.now())
+		db.session.add(update)
+		db.session.commit()
 
 
-# class UserUpdate(factory.alchemy.SQLAlchemyModelFactory):
-# 	"""Creates fake updates from fake users"""
-
-
-
-#to use:
-#ipython -i factory_set_up.py 
-#connect_to_db(app)
-#UserFactory.create()
-# do as many times as needed
-#db.session.commit()
