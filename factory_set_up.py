94aa6d6f9483dd5e16fe04b4dc2bda6996b92924
Talia Trilling <taliatrilling@gmail.com>
2016-11-07 20:05:25 +0000

    changing from factory boy to faker

--- factory_set_up.py
+++ factory_set_up.py
@@ -1,31 +1,41 @@
 from model import User, Update, Comment, Pair, Message, Request, connect_to_db, db
-import factory
+# import factory
 from datetime import datetime
 from server import app
 
 
 
-class UserFactoryPublic(factory.alchemy.SQLAlchemyModelFactory):
-	""" """
-	class Meta:
-		model = User
-		sqlalchemy_session = db.session
+# class UserFactoryPublic(factory.alchemy.SQLAlchemyModelFactory):
+# 	"""Creates fake users with profiles set to public"""
 
-	username = factory.Sequence(lambda n: "username%d" % n)
-	password = "password123"
-	joined_at = factory.LazyFunction(datetime.now)
-	is_public = True
+# 	class Meta:
+# 		model = User
+# 		sqlalchemy_session = db.session
 
-class UserFactoryPrivate(factory.alchemy.SQLAlchemyModelFactory):
-	""" """
-	class Meta:
-		model = User
-		sqlalchemy_session = db.session
+# 	username = factory.Sequence(lambda n: "username%d" % n)
+# 	password = "password123"
+# 	joined_at = factory.LazyFunction(datetime.now)
+# 	is_public = True
 
-	username = factory.Sequence(lambda n: "username%d" % n)
-	password = "password123"
-	joined_at = factory.LazyFunction(datetime.now)
-	is_public = False
+# class UserFactoryPrivate(factory.alchemy.SQLAlchemyModelFactory):
+# 	"""Creates fake users with profiles set to private"""
+# 	class Meta:
+# 		model = User
+# 		sqlalchemy_session = db.session
+
+# 	username = factory.Sequence(lambda n: "username%d" % n)
+# 	password = "password123"
+# 	joined_at = factory.LazyFunction(datetime.now)
+# 	is_public = False
+
+# class UserUpdatePublic(factory.alchemy.SQLAlchemyModelFactory):
+# 	"""Creates fake updates from users with public profiles"""
+# 	pass
+
+# class UserUpdatePrivate(factory.alchemy.SQLAlchemyModelFactory):
+# 	"""Creates fake updates from users with private profiles"""
+# 	pass
+
 
 #to use:
 #ipython -i factory_set_up.py 
